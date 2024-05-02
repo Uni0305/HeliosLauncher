6971971baa855ffd0ba4dc79fb0d5b37f6dbfc68
Uni0305 <git@uni0305.me>
2024-05-03 01:43:47 +0900

    Added todo comments to update distribution url

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -2,6 +2,7 @@ const { DistributionAPI } = require('helios-core/common')
 
 const ConfigManager = require('./configmanager')
 
+// TODO: Change this to your own distribution URL.
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
 exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
